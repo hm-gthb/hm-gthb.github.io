796204db09c32afc6b050888bad3956adfb2dfc5
hyipmilker <64599811+hyipmilker@users.noreply.github.com>
2020-05-01 10:31:17 +0300

    Update core.js

--- js/core.js
+++ js/core.js
@@ -5,6 +5,40 @@
     }
 });
 
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  var username = getCookie("username");
+  if (username != "") {
+   alert("Welcome again " + username);
+  } else {
+    username = prompt("Please enter your name:", "");
+    if (username != "" && username != null) {
+      setCookie("username", username, 365);
+    }
+  }
+}
 
 $("#login_form").submit(function(e){
     e.preventDefault();
@@ -35,3 +69,8 @@ $("#forgot_form").submit(function(e){
       if(return_data.err){$("#password_return").addClass("text-danger").html(return_data.msg);}else{$("#password_return").removeClass("text-danger").addClass("text-success").html(return_data.msg);setTimeout(function(){window.location="/account.html"},5000);}
     },"json");
 });
+
+// Tooltips Initialization
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip()
+})
