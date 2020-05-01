7128052ff768c68c6066e05a27d715e8a9e462c6
hyipmilker <64599811+hyipmilker@users.noreply.github.com>
2020-05-01 19:27:51 +0300

    up

--- js/core.js
+++ js/core.js
@@ -48,6 +48,21 @@ function checkCookie() {
   }
 }
 
+function copyClip(el) {
+  /* Get the text field */
+  var copyText = document.getElementById(el);
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
+
 $(document).on("submit", "#login_form", function (e) {
     e.preventDefault();
     var form = this;
