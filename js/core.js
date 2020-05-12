8d286b15fad60a7acd0d8fa816905238305dc2ba
hyipmilker <64599811+hyipmilker@users.noreply.github.com>
2020-05-12 10:35:30 +0300

    up

--- js/core.js
+++ js/core.js
@@ -329,6 +329,8 @@ $(document).on("submit", "#withdraw_form", function (e) {
 
     }else{
       $("#withdraw_return").addClass("text-danger").html("Insufficient funds.");
+      alert(requested);
+      alert(all_avail);
     }
 
     
